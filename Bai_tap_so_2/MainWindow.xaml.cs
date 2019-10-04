fa3d2a636ba62f6d6f85533e23d86d537fa0556d
tiend <tiend@DESKTOP-BFEV047>
2019-10-04 11:43:07 +0700

    Add project files.

---
+++ Bai_tap_so_2/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bai_tap_so_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void typeSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem selectedType = (type.SelectedItem as ListBoxItem);
+            switch (selectedType.Content.ToString())
+            {
+                case "int":
+                    showIntValue();
+                    break;
+            }
+        }
+        private void showIntValue()
+        {
+             int intVar;
+             intVar = 42;
+             value.Text = intVar.ToString();
+        }
+        private void QuitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
